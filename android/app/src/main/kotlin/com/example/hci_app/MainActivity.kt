7f9836feb404592e57db866ef29cd06305829780
malware4 <nickzerz92@hotmail.com>
2022-01-07 23:58:03 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/hci_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hci_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,53 @@
+//Packages
+import 'package:flutter/material.dart';
+
+//Pages
+import '../pages/chats_page.dart';
+import '../pages/users_page.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _HomePageState();
+  }
+}
+
+class _HomePageState extends State<HomePage> {
+  int _currentPage = 0;
+  final List<Widget> _pages = [
+    ChatsPage(),
+    UsersPage(),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return _buildUI();
+  }
+
+  Widget _buildUI() {
+    return Scaffold(
+      body: _pages[_currentPage],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentPage,
+        onTap: (_index) {
+          setState(() {
+            _currentPage = _index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            label: "Chats",
+            icon: Icon(
+              Icons.chat_bubble,
+            ),
+          ),
+          BottomNavigationBarItem(
+            label: "Users",
+            icon: Icon(
+              Icons.supervisor_account_sharp,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
